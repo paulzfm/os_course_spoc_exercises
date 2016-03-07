01122a1ba1b3eaef2afd3dcd43b0488d1b9dadc7
Paul Zhu <zhufengminpaul@163.com>
2016-03-07 13:52:25 +0800

    update

--- FirstFit/list.c
+++ FirstFit/list.c
@@ -3,7 +3,7 @@
 void list_init(struct free_list *list)  // initialize
 {
     list->prev = list->next = list;
-    list->size = MAX_MEM;
+    list->size = 0;
 }
 
 void list_insert(struct free_list *node, struct free_list *list) // insert node as list's next
