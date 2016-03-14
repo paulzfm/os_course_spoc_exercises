a384fccad93b71e558b66e4d03b6e0249d5a9cda
Paul Zhu <zhufengminpaul@163.com>
2016-03-14 09:49:13 +0800

    update cases

--- Translation/trans.py
+++ Translation/trans.py
@@ -1,5 +1,4 @@
-
-def trans(pdbr, va, pa):
+def trans(va, pa):
     pde_idx = va >> 20
     pte_idx = (va >> 12) & 0xFF
     pt = pa >> 12
@@ -8,4 +7,14 @@ def trans(pdbr, va, pa):
     pde_cxt = (pt << 12) & 0b000000000011
     print 'va 0x%08x, pa 0x%08x, pde_idx 0x%08x, pde_ctx 0x%08x, pte_idx 0x%08x, pte_ctx 0x%08x' % (va, pa, pde_idx, pde_cxt, pte_idx, pte_cxt)
 
-trans(0, 0xcd82c07c, 0x0c20907c)
+if __name__ == '__main__':
+    trans(0xc2265b1f, 0x0d8f1b1f)
+    trans(0xcc386bbc, 0x0414cbbc)
+    trans(0xc7ed4d57, 0x07311d57)
+    trans(0xca6cecc0, 0x0c9e9cc0)
+    trans(0xc18072e8, 0x007412e8)
+    trans(0xcd5f4b3a, 0x06ec9b3a)
+    trans(0xcc324c99, 0x0008ac99)
+    trans(0xc7204e52, 0x0b8b6e52)
+    trans(0xc3a90293, 0x0f1fd293)
+    trans(0xce6c3f32, 0x007d4f32)
