e16fbffe2652b1fa25e172d9f264d4d07f7fb447
Xinrui ZHU <452075061@qq.com>
2016-03-14 10:53:52 +0800

    TransBugFixed by Milesian

---
+++ README副本.md
@@ -0,0 +1,8 @@
+Virtual Address 0x2c03:
+->pde index: 0x05 pde contents:(valid 1, pfn 0x45)
+->pte index: 0x08 pte contents:(valid 0, pfn 0x7f)
+-> Fault (page table entry not valid)
+
+Virtual Address 0x390e:
+->pde index: 0x0e pde contents:(valid 0, pfn 0x7f)
+-> Fault (page directory entry no valid)
--- Translation/trans.py
+++ Translation/trans.py
@@ -1,10 +1,10 @@
 def trans(va, pa):
-    pde_idx = va >> 20
-    pte_idx = (va >> 12) & 0xFF
+    pde_idx = va >> 22
+    pte_idx = (va >> 12) & 0b1111111111
     pt = pa >> 12
     pte_cxt = (pt << 12) & 0b000000000011
-    pt = pde_idx - 0x300 + 1
-    pde_cxt = (pt << 12) & 0b000000000011
+    pde_cxt = (pde_idx - 0x300 + 1) << 12 | 0x03
+    #pde_cxt = (pt << 12) & 0b000000000011
     print 'va 0x%08x, pa 0x%08x, pde_idx 0x%08x, pde_ctx 0x%08x, pte_idx 0x%08x, pte_ctx 0x%08x' % (va, pa, pde_idx, pde_cxt, pte_idx, pte_cxt)
 
 if __name__ == '__main__':
